b8488e20220fc4d712b6f55fa6829341654c43b4
Greytiger23 <isabelmothoa49@gmail.com>
2024-07-24 18:24:30 -0700

    react intro

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import holbertonLogo from './holberton-logo.png';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button>OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+    </div>
+  );
+}
+
+export default App;
