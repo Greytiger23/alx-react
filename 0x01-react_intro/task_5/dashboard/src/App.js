5bf720d020e65f93c38018a3705804cbaa36596b
Greytiger23 <isabelmothoa49@gmail.com>
2024-07-24 19:47:41 -0700

    react intro

---
+++ 0x01-react_intro/task_5/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email: </label>
+        <input type="email" id="email" name="email" />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button>OK</button>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
