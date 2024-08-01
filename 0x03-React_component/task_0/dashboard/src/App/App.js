5f035025331830c87ccb1b3257174e6773b73115
Greytiger23 <isabelmothoa49@gmail.com>
2024-07-31 17:09:53 -0700

    react component

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import Notifications from '../Notifications/Notifications';
+import './App.css';
+
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+];
+
+class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    isLoggedIn: false,
+  };
+
+  render() {
+    return (
+      <>
+        <Notifications displayDrawer={false} listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> <Login />}
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ 0x03-React_component/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import NotificationItem from './NotificationItem';
+import NotificationItemShape from './NotificationItemShape';
+import './Notifications.css';
+
+const Notifications = ({ displayDrawer, listNotifications }) => {
+  return (
+    <>
+      <div className="menuItem">
+        Your notifications
+      </div>
+      {displayDrawer && (
+        <div className="Notifications">
+          {listNotifications.length === 0 ? (
+            <p>No new notification for now</p>
+          ) : (
+            <>
+              <p>Here is the list of notifications</p>
+              <ul>
+                {listNotifications.map((notification) => (
+                  <NotificationItem
+                    key={notification.id}
+                    type={notification.type}
+                    value={notification.value}
+                    html={notification.html}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+export default Notifications;
