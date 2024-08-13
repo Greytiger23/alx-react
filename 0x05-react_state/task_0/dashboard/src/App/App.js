d539ea8ed24a8c9bd288feb442f6e282812cae11
Greytiger23 <isabelmothoa49@gmail.com>
2024-08-12 18:47:42 -0700

    react state

--- 0x05-react_state/task_0/dashboard/src/App/App.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -25,6 +25,7 @@ class App extends Component {
     this.state = {
       displayDrawer: false,
     };
+
     this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
     this.handleHideDrawer = this.handleHideDrawer.bind(this);
   }
@@ -46,11 +47,9 @@ class App extends Component {
           handleDisplayDrawer={this.handleDisplayDrawer}
           handleHideDrawer={this.handleHideDrawer}
         />
-        <footer className={css(styles.footer)}>
-          Copyright 2020 - Holberton School
-        </footer>
       </div>
     );
+  }
 }
 
 export default App;
