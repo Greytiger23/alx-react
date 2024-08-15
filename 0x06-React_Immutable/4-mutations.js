b65bad558f172eead3647e2fd94479d05fd93692
Greytiger23 <isabelmothoa49@gmail.com>
2024-08-15 16:59:57 -0700

    react immutable

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -9,8 +9,8 @@ const map = Map({
   6: 'Lucas',
 });
 
-const map2 = map.withMutations(values) => {
+const map2 = map.withMutations((values) => {
   values.set(2, 'Benjamin').set(4, 'Oliver');
-}
+});
 
 export { map, map2 };
