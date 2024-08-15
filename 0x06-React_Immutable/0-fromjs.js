b4e9f5a7ce9b765502c8ccd14190c8fc16f01c1f
Greytiger23 <isabelmothoa49@gmail.com>
2024-08-14 17:29:35 -0700

    react immutable

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,13 +3,3 @@ import { fromJS } from 'immutable';
 function getImmutableObject(object) {
   return fromJS(object);
 }
-
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-const immutableMap = getImmutableObject(obj);
-console.log(immutableMap);
