62d73a27d74113153b095921433c91a399bb6a42
Greytiger23 <isabelmothoa49@gmail.com>
2024-08-14 18:05:13 -0700

    react immutable

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -9,11 +9,3 @@ export function mergeElements(page1, page2) {
   const map2 = Map(page2);
   return map1.merge(map2).toList();
 }
-
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
-console.log(concatElements(array1, array2));
-
-const obj1 = { a: 1, b: 2 };
-const obj2 = { b: 3, c: 4 };
-console.log(mergeElements(obj1, obj2));
