a66e7c5ced9997c694d6b40b423aba54ac495b9f
Greytiger23 <isabelmothoa49@gmail.com>
2024-08-15 15:22:49 -0700

    react immutable

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -4,10 +4,11 @@ export function printBestStudents(grades) {
   const bestStudents = Seq(grades)
     .filter(student => student.score >= 70)
     .map(student => ({
-      ...student,
       firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
     }));
 
-  console.log(bestStudents.toJS());
+  bestStudents.forEAch(bestStudent => {
+    console.log(`First Name: ${bestStudent.firstName}, Last Name: ${bestStudent.lastName}`);
+  }
 }
