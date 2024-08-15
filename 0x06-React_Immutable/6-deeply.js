012be6d5c1c72dad4364b7c5dcc78f62abf6de36
Greytiger23 <isabelmothoa49@gmail.com>
2024-08-14 18:19:39 -0700

    react immutable

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,31 +1,5 @@
 import { Map } from 'immutable';
 
 export function mergeDeeplyElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-  return map1.mergeDeep(map2).toList();
+  return Map(page1).mergeDeep(page2);
 }
-
-const page1 = {
-  'user-1': {
-    id: 1,
-    name: 'test',
-    likes: {
-      1: {
-        uid: 1234,
-      }
-    }
-  },
-};
-
-const page2 = {
-  'user-1': {
-    likes: {
-      2: {
-        uid: 134,
-      }
-    }
-  },
-};
-
-console.log(mergeDeeplyElements(page1, page2).toJS());
