d80da64435f2ef53fbddad9ea106566415015db4
Greytiger23 <isabelmothoa49@gmail.com>
2024-08-15 17:22:15 -0700

    react immutable

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,13 +1,12 @@
 import { Seq } from 'immutable';
 
-
 export default function printBestStudents(grades) {
   const students = Seq(grades)
     .filter(student => student.score >= 70)
     .map(student => ({
       score: student.score,
-      firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-      lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
+      firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1).toLowerCase(),
+      lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1).toLowerCase(),
     }));
 
   console.log(students.toJS());
